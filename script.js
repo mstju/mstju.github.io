a8ebaffe76a7e9c82f20debd95d05bf4a906d946
Laura Segouras <lausegouras@yahoo.com.br>
2022-11-09 19:06:44 -0300

    js - bairros

---
+++ script.js
@@ -0,0 +1,25 @@
+(async function(){
+    try{    
+        const response = await fetch('https://servicodados.ibge.gov.br/api/v1/localidades/municipios/3550308/distritos')
+        const jsonData = await response.json()
+        console.log(jsonData)
+    }catch(e){
+        console.log('Deu erro!')
+    }    
+})()
+
+
+
+/*OUTRA FORMA
+
+const response = fetch('https://servicodados.ibge.gov.br/api/v1/localidades/municipios/3550308/distritos')
+        .then(responseData => {
+            return responseData.json()
+        })
+        .then(jsonData =>{
+            console.log(jsonData)
+        })
+        .catch (function (e){
+            console.log("Deu erro!")
+        })
+*/
