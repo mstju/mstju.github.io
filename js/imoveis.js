2b57607c7e578d66d6d79597f1416efc4a8f73fa
Laura Segouras <lausegouras@yahoo.com.br>
2022-12-19 15:20:50 -0300

    js inputs validation - imoveis page

--- js/imoveis.js
+++ js/imoveis.js
@@ -1,89 +1,98 @@
-/*class Validator{
-
-    constructor(){
-        this.validations = [
-            'data-min-length',
-        ]
-    }
-
-    // iniciar a validação de todos os campos
-    validate(form){
-    
-    // pegar todos os inputs
-        let inputs = document.querySelectorAll('input');
-      
-
-    // HTML Collection => array 
-        let inputsArray = [...inputs];
-
-    // loop dos inputs e validação do que for encontrado    
-        inputsArray.forEach(function(input){
-    
-    // loop de todas as validações existentes
-            for(let i = 0; this.validations.length > i; i++){
-                if(input.getAttribute(this.validations[i]) != null){
-                    console.log(input.getAttribute());
-                    console.log('achou validação');
-                }
-            }
-        }, this);
-      
-    }
-}
-
-let form = document.getElementById("form-container");
-let submit = document.getElementById("btn-submit");
-
-let validator = new Validator();
-
-// evento que dispara as validações
-
-submit.addEventListener('click', function(e){
-    e.preventDefault();
-    validator.validate(form);
-});*/
-
 // Função para validar inputs
 
-
-function sendbtn(){
-    $("#rent-form").submit(function(event){
-        event.preventDefault();
-        let email = document.getElementById('email').value
-        let name = document.getElementById('name').value
-        let lastname = document.getElementById('lastname').value
-        let phone = document.getElementById('phone').value
-        let message = document.getElementById('message').value
-        let infos = document.getElementById('infos')
-        let regex = /^[0-9]+$/
-
-        if(name.match(regex)){
-            return infos.innerHTML = `<h4 style = "color: red"> O nome deve conter apenas letras!</h4>`
-        }
-
-        if(name == "" || name < 3){
-            return infos.innerHTML = `<h4 style = "color: red"> O nome deve conter pelo menos 3 letras!</h4>`
-        }
-
-        if(lastname == "" || lastname < 3){
-            return infos.innerHTML = `<h4 style = "color: red"> O sobrenome deve conter pelo menos 3 letras!</h4>`
-        }
-
-        if(email == "" || phone == ""){
-            return infos.innerHTML = `<h4 style = "color: red"> Por favor, preencha esse campo!</h4>`
-        }
-
-        if(phone.length <= 14){
-            return infos.innerHTML = `<h4 style = "color: red"> O número do celular deve conter 11 dígitos, contando com DDD!</h4>`
-        }
-
-        if(message.length <= 5 || message == ""){
-            return infos.innerHTML = `<h4 style = "color: red"> Este campo deve conter no mínimo 5 caracteres! </h4>`
-        }
-    
-    (this).submit()
-
-    })
+let form = document.getElementById('rent-form');
+if (form.addEventListener) {
+    form.addEventListener('submit', validateForm);
+}else if(form.attachEvent) {
+    form.attachEvent('onsubmit', validateForm);
 }
 
-//Função para validar input de telefone*/
+function validateForm(evt) {
+    let email = document.getElementById('email');
+    let name = document.getElementById('name');
+    let lastname = document.getElementById('lastname');
+    let phone = document.getElementById('phone');
+    let message = document.getElementById('message');
+    let agreement = document.getElementById('agreement');
+    let filterEmail = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+	let filterPhone = /^(?:\()[0-9]{2}(?:\))\s?[0-9]{5}(?:-)[0-9]{4}$/;
+    let contError = 0;
+
+    let box_email = document.querySelector('.msg-email')
+    if(email.value == ""){
+        box_email.innerHTML = 'Favor preencher o E-mail!';
+        box_email.style.display = 'block';
+        contError += 1;
+    }else if(filterEmail.test(email.value)){
+        box_email.style.display = 'none';
+    }else{
+        box_email.innerHTML = 'Formato do E-mail inválido!';
+        box_email.style.display = 'block';
+        contError += 1;
+    }
+
+    let box_name = document.querySelector('.msg-name')
+    if(name.value == ""){
+        box_name.innerHTML = 'Favor preencher o nome!';
+        box_name.style.display = 'block';
+        contError += 1;
+    }else if(name.length >= 3){
+        box_name.style.display = 'none';
+    }else{
+        box_name.innerHTML = 'O nome deve conter mais de 3 caracteres!';
+        box_name.style.display = 'block';
+        contError += 1;
+    }
+
+    let box_lastname = document.querySelector('.msg-lastname')
+    if(lastname.value == ""){
+        box_lastname.innerHTML = 'Favor preencher o sobrenome!';
+        box_lastname.style.display = 'block';
+        contError += 1;
+    }else if(lastname.length >= 3){
+        box_lastname.style.display = 'none';
+    }else{
+        box_lastname.innerHTML = 'O sobrenome deve conter mais de 3 caracteres!';
+        box_lastname.style.display = 'block';
+        contError += 1;
+    }
+
+    let box_phone = document.querySelector('.msg-phone')
+    if(phone.value == ""){
+        box_phone.innerHTML = 'Favor preencher o telefone celular!';
+        box_phone.style.display = 'block';
+        contError += 1;
+    }else if(filterPhone.test(phone.value)){
+        box_phone.style.display = 'none';
+    }else{
+        box_phone.innerHTML = 'Formato do telefone inválido! Favor preencher nesse formato: (XX)XXXXX-XXXX.';
+        box_phone.style.display = 'block';
+        contError += 1;
+    }
+
+    let box_message = document.querySelector('.msg-message')
+    if(message.value == ""){
+        box_message.innerHTML = 'Favor preencher a mensagem!';
+        box_message.style.display = 'block';
+        contError += 1;
+    }else if(message.length >= 5){
+        box_message.style.display = 'none';
+    }else{
+        box_message.innerHTML = 'A mensagem deve conter mais de 5 caracteres';
+        box_message.style.display = 'block';
+        contError += 1;
+    }
+
+    let box_agreement = document.querySelector('.msg-agreement')
+    if(agreement.value == ""){
+        box_agreement.innerHTML = 'Favor clicar na caixa para aceitar a licença de uso!';
+        box_agreement.style.display = 'block';
+        contError += 1;
+    }else{
+        box_agreement.style.display = 'none';
+    }
+
+    if (contError > 0) {
+        evt.preventDefault();
+    }
+}
